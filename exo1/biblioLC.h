61750b371d4289925bc9888d269a2bffbde7316b
hugoamadoUPMC <hugoamado1007@gmail.com>
2024-02-21 23:15:22 +0100

    ajout de prototype dans .h

--- exo1/biblioLC.h
+++ exo1/biblioLC.h
@@ -17,11 +17,12 @@ void liberer_livre(Livre* l);
 Biblio* creer_biblio();
 void liberer_biblio(Biblio* b);
 void inserer_en_tete(Biblio* b, int num, char* titre, char* auteur);
+void afficher_livre(Livre *l);
+void afficher_biblio(Biblio* b);
 Livre* recherche_par_num(Biblio* b, int n);
 Livre* recherche_par_titre(Biblio* b, char* titre);
 Biblio* recherche_livres_auteur(Biblio* b, char* auteur);
 void supprimer_livre(Biblio* b, int num, char* titre, char* auteur);
-
-
+Biblio* fusion_biblio(Biblio* b1, Biblio* b2);
 
 #endif
